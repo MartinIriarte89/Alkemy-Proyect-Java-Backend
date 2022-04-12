7c34585e6216002ea2936c1cd4421b79faaffbcc
Mostrador <mostrador@ladoce.lp>
2022-04-12 13:57:37 -0300

    Primer commit.

---
+++ src/main/java/com/disneyapi/AlkemyProyectJavaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.disneyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlkemyProyectJavaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlkemyProyectJavaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/disneyapi/AlkemyProyectJavaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disneyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlkemyProyectJavaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
