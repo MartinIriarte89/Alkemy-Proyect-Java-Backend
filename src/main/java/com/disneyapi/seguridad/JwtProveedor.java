b529508a2d0e4978f8d7ab602e69f43812226879
Mostrador <mostrador@ladoce.lp>
2022-04-14 07:55:09 -0300

    Se agrega la configuracion de seguridad

--- src/main/java/com/disneyapi/seguridad/JwtProveedor.java
+++ src/main/java/com/disneyapi/seguridad/JwtProveedor.java
@@ -1,6 +1,6 @@
 package com.disneyapi.seguridad;
 
-import java.util.Date;
+/*import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -92,4 +92,4 @@ public class JwtProveedor {
 			log.info("Token JWT no soportado: " + e.getMessage());
 		}
 	}
-}
+}*/
