ef8f17b438998c91bb309044c448e27e2caf9fda
Mostrador <mostrador@ladoce.lp>
2022-04-16 10:49:50 -0300

    Se agrega la configuracion necesaria para swagger.

--- src/main/java/com/disneyapi/config/MiConfiguracion.java
+++ src/main/java/com/disneyapi/config/MiConfiguracion.java
@@ -8,7 +8,17 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @Configuration
+@EnableSwagger2
 public class MiConfiguracion {
 
 	@Bean
@@ -33,5 +43,24 @@ public class MiConfiguracion {
 						.maxAge(3600);
 			}
 		};
+	}
+	
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.disneyapi.controlador"))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(apiInfo());
+	}
+	
+	public ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("Disney Api LATAM")
+				.description("Este proyecto es un proyecto para ser presentado en Alkemy.")
+				.version("1.0")
+				.contact(new Contact("Martin Iriarte", "https://github.com/MartinIriarte89", "iriartemartin2@gmail.com"))
+				.build();
 	}
 }
